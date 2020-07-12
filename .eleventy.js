fd0ae84be37ace30f921284a31f36466dcfff482
Thomas Tuvignon <thomas.tuvignon@icloud.com>
2020-07-12 23:51:19 +0200

    🏗 Refactor PHP to Eleventy

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("videos");
+
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+};
