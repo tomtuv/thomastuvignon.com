f4a232baaac40c9c745fe27861e117c75a7f116a
Thomas Tuvignon <thomas.tuvignon@icloud.com>
2020-07-14 13:16:24 +0200

    🧹 Clean up

--- src/js/script.js
+++ src/js/script.js
@@ -14,33 +14,24 @@ AOS.init({
 // Bubbles
 
 $(window).bind("scroll", function () {
-  parallax_scroll();
-});
-
-function parallax_scroll() {
   var scrolled = $(window).scrollTop();
+
   $("#parallax-1").css("top", 0 - scrolled * 0.1 + "px");
   $("#parallax-2").css("top", 0 - scrolled * 0.2 + "px");
   $("#parallax-3").css("top", 0 - scrolled * 0.3 + "px");
   $("#parallax-4").css("top", 0 - scrolled * 0.4 + "px");
-}
+});
 
 // Video
 
-function toggle_video_modal() {
-  $(".js-trigger-modal").on("click", function (event) {
-    event.preventDefault();
-    $(".video").fadeIn(250);
-    $("body").addClass("show-video-modal");
-    $("video")[0].play();
-  });
-  $("body").on("click", "body, .close-video-modal, #video .overlay", function (
-    event
-  ) {
-    event.preventDefault();
-    $("body").removeClass("show-video-modal");
-    $(".video").fadeOut(250);
-    $("video")[0].pause();
-  });
-}
-toggle_video_modal();
+$("[data-open-modal]").on("click", function (event) {
+  event.preventDefault();
+  $(".video").fadeIn(250);
+  $("video")[0].play();
+});
+
+$("[data-close-modal]").on("click", function (event) {
+  event.preventDefault();
+  $(".video").fadeOut(250);
+  $("video")[0].pause();
+});
