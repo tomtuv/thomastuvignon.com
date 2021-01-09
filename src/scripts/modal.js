4323689244569af8ede48be7eac7f5b2e628d21c
Thomas Tuvignon <thomas.tuvignon@icloud.com>
2021-01-09 16:04:26 +0100

    🐛 Fix modal script

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,16 +1,15 @@
 export default function Modal() {
-  document
-    .querySelector("[data-open-modal]")
-    .addEventListener("click", function (event) {
-      event.preventDefault();
+  const openButton = document.querySelector("[data-open-modal]");
+  const closeButton = document.querySelector("[data-close-modal]");
+
+  openButton &&
+    openButton.addEventListener("click", function () {
       document.querySelector(".video").classList.add("active");
       document.querySelector("video").play();
     });
 
-  document
-    .querySelector("[data-close-modal]")
-    .addEventListener("click", function (event) {
-      event.preventDefault();
+  closeButton &&
+    closeButton.addEventListener("click", function () {
       document.querySelector(".video").classList.remove("active");
       document.querySelector("video").pause();
     });
