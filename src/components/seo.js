65cd54e8c4942a576cbb67aa335357b0a1d40a99
Thomas Tuvignon <thomas.tuvignon@icloud.com>
2021-01-16 23:46:38 +0100

    🔍 Update SEO component

--- src/components/seo.js
+++ src/components/seo.js
@@ -30,33 +30,25 @@ const SEO = ({ title, description, image, article }) => {
       titleTemplate={pathname !== "/" ? titleTemplate : seo.title}
       htmlAttributes={{ lang: "fr" }}
     >
+      <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+      <link rel="icon" href="favicon.png" />
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
-
       {seo.url && <meta property="og:url" content={seo.url} />}
-
       {(article ? true : null) && <meta property="og:type" content="article" />}
-
       {seo.title && <meta property="og:title" content={seo.title} />}
-
       {seo.description && (
         <meta property="og:description" content={seo.description} />
       )}
-
       {seo.image && <meta property="og:image" content={seo.image} />}
-
       <meta name="twitter:card" content="summary_large_image" />
-
       {twitterUsername && (
         <meta name="twitter:creator" content={twitterUsername} />
       )}
-
       {seo.title && <meta name="twitter:title" content={seo.title} />}
-
       {seo.description && (
         <meta name="twitter:description" content={seo.description} />
       )}
-
       {seo.image && <meta name="twitter:image" content={seo.image} />}
     </Helmet>
   );
