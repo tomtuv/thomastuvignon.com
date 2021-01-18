cbe69d0b71afb6472aa3dda2b914f3117bb04077
Thomas Tuvignon <thomas.tuvignon@icloud.com>
2021-01-18 01:20:44 +0100

    🗑 Clean up JSX

--- src/templates/project.js
+++ src/templates/project.js
@@ -40,35 +40,33 @@ const ProjectTemplate = ({ data }) => {
                       )}
                     </div>
                   ) : (
-                    <>
-                      {block.images.map((image, i) => (
-                        <div
-                          className={
-                            block.layout === "2 columns"
-                              ? "col-md-6"
-                              : block.layout === "3 columns"
-                              ? "col-md-4"
-                              : "col"
-                          }
-                          data-aos="fade-up"
-                          key={i}
-                        >
-                          <figure>
-                            <Img
-                              fixed={image.fixed}
-                              alt={project.title}
-                              style={{
-                                display: "block",
-                                width: "100%",
-                                height: "auto",
-                              }}
-                              imgStyle={{ position: "relative" }}
-                              placeholderStyle={{ position: "absolute" }}
-                            />
-                          </figure>
-                        </div>
-                      ))}
-                    </>
+                    block.images.map((image, i) => (
+                      <div
+                        className={
+                          block.layout === "2 columns"
+                            ? "col-md-6"
+                            : block.layout === "3 columns"
+                            ? "col-md-4"
+                            : "col"
+                        }
+                        data-aos="fade-up"
+                        key={i}
+                      >
+                        <figure>
+                          <Img
+                            fixed={image.fixed}
+                            alt={project.title}
+                            style={{
+                              display: "block",
+                              width: "100%",
+                              height: "auto",
+                            }}
+                            imgStyle={{ position: "relative" }}
+                            placeholderStyle={{ position: "absolute" }}
+                          />
+                        </figure>
+                      </div>
+                    ))
                   )}
                 </div>
               </section>
