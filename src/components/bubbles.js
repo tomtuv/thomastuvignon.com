799f51b7c9e20a94e3aa858a97870322f43eabdb
Thomas Tuvignon <thomas.tuvignon@icloud.com>
2021-01-24 21:30:10 +0100

    💄 Update bubbles styles

--- src/components/bubbles.js
+++ src/components/bubbles.js
@@ -8,16 +8,16 @@ const Bubbles = () => {
         window.scrollTop ||
         document.querySelector("html").scrollTop;
 
-      document.querySelector('[data-parallax="1"]').style = `top: ${
+      document.querySelector(".bubbles-parallax-1").style = `top: ${
         0 - scrolled * 0.1
       }px`;
-      document.querySelector('[data-parallax="2"]').style = `top: ${
+      document.querySelector(".bubbles-parallax-2").style = `top: ${
         0 - scrolled * 0.2
       }px`;
-      document.querySelector('[data-parallax="3"]').style = `top: ${
+      document.querySelector(".bubbles-parallax-3").style = `top: ${
         0 - scrolled * 0.3
       }px`;
-      document.querySelector('[data-parallax="4"]').style = `top: ${
+      document.querySelector(".bubbles-parallax-4").style = `top: ${
         0 - scrolled * 0.4
       }px`;
     });
@@ -25,160 +25,64 @@ const Bubbles = () => {
 
   return (
     <div className="bubbles" aria-hidden="true">
-      <div className="bubbles-parallax" data-parallax="1">
-        <div className="bubbles-item" data-bubble="1">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="2">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="3">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="4">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="5">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="6">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="7">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="8">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="9">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="10">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="11">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="12">
-          &nbsp;
-        </div>
+      <div className="bubbles-parallax bubbles-parallax-1">
+        <div className="bubbles-item bubbles-item-1">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-2">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-3">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-4">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-5">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-6">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-7">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-8">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-9">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-10">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-11">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-12">&nbsp;</div>
       </div>
 
-      <div className="bubbles-parallax" data-parallax="2">
-        <div className="bubbles-item" data-bubble="13">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="14">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="15">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="16">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="17">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="18">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="19">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="20">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="21">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="22">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="23">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="24">
-          &nbsp;
-        </div>
+      <div className="bubbles-parallax bubbles-parallax-2">
+        <div className="bubbles-item bubbles-item-13">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-14">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-15">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-16">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-17">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-18">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-19">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-20">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-21">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-22">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-23">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-24">&nbsp;</div>
       </div>
 
-      <div className="bubbles-parallax" data-parallax="3">
-        <div className="bubbles-item" data-bubble="25">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="26">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="27">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="28">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="29">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="30">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="31">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="32">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="33">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="34">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="35">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="36">
-          &nbsp;
-        </div>
+      <div className="bubbles-parallax bubbles-parallax-3">
+        <div className="bubbles-item bubbles-item-25">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-26">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-27">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-28">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-29">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-30">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-31">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-32">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-33">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-34">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-35">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-36">&nbsp;</div>
       </div>
 
-      <div className="bubbles-parallax" data-parallax="4">
-        <div className="bubbles-item" data-bubble="37">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="38">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="39">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="40">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="41">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="42">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="43">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="44">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="45">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="46">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="47">
-          &nbsp;
-        </div>
-        <div className="bubbles-item" data-bubble="48">
-          &nbsp;
-        </div>
+      <div className="bubbles-parallax bubbles-parallax-4">
+        <div className="bubbles-item bubbles-item-37">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-38">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-39">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-40">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-41">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-42">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-43">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-44">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-45">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-46">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-47">&nbsp;</div>
+        <div className="bubbles-item bubbles-item-48">&nbsp;</div>
       </div>
     </div>
   );
