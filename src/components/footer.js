33315c5c4c20d7188bfd368d8d24da8c150efc7e
Thomas Tuvignon <thomas.tuvignon@icloud.com>
2021-01-24 21:17:57 +0100

    ✨ Add Material icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,45 +1,72 @@
 import React from "react";
 
+import Linkedin from "./linkedin";
+import Twitter from "./twitter";
+import Facebook from "./facebook";
+import Instagram from "./instagram";
+import Github from "./github";
+import Mail from "./mail";
+
 const Footer = () => (
   <footer className="footer">
     <div className="container">
       <ul>
         <li>
           <a
+            className="icon icon-linkedin"
             href="https://www.linkedin.com/in/thomastuvignon"
             target="_blank"
             rel="noreferrer"
           >
-            <i className="fa fa-linkedin" aria-hidden="true"></i>
-            <span>LinkedIn</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/tomtuv" target="_blank" rel="noreferrer">
-            <i className="fa fa-twitter" aria-hidden="true"></i>
-            <span>Twitter</span>
+            <Linkedin />
           </a>
         </li>
         <li>
           <a
+            className="icon icon-twitter"
+            href="https://twitter.com/tomtuv"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Twitter />
+          </a>
+        </li>
+        <li>
+          <a
+            className="icon icon-facebook"
+            href="https://www.facebook.com/thomas.tuvignon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Facebook />
+          </a>
+        </li>
+        <li>
+          <a
+            className="icon icon-instagram"
             href="https://www.instagram.com/tomtuv"
             target="_blank"
             rel="noreferrer"
           >
-            <i className="fa fa-instagram" aria-hidden="true"></i>
-            <span>Instagram</span>
+            <Instagram />
           </a>
         </li>
         <li>
-          <a href="https://github.com/tomtuv" target="_blank" rel="noreferrer">
-            <i className="fa fa-github" aria-hidden="true"></i>
-            <span>GitHub</span>
+          <a
+            className="icon icon-github"
+            href="https://github.com/tomtuv"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Github />
           </a>
         </li>
         <li>
-          <a href="mailto:thomas.tuvignon@icloud.com">
-            <i className="fa fa-envelope" aria-hidden="true"></i>
-            <span>E-mail</span>
+          <a
+            className="icon icon-mail"
+            href="mailto:contact@thomastuvignon.com"
+          >
+            <Mail />
           </a>
         </li>
       </ul>
