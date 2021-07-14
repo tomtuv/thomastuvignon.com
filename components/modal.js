c6a590ff04c9480ca6d7b38bbe6bebaf86df0d5b
Thomas Tuvignon <thomas.tuvignon@ultro.fr>
2021-07-15 00:59:33 +0200

    💄 Update modal styles

--- components/modal.js
+++ components/modal.js
@@ -13,21 +13,17 @@ export default function Modal({ modal, handleClick, video, videoUrl }) {
         <Close />
       </button>
       <div className="container">
-        <div className="grid">
-          <div data-column="12" data-column-lg="10" data-start-lg="2">
-            <video
-              width="1920"
-              height="1080"
-              controls
-              playsInline
-              preload="none"
-              poster="/og-image.jpg"
-              ref={video}
-            >
-              <source src={videoUrl} type="video/mp4" />
-            </video>
-          </div>
-        </div>
+        <video
+          width="1920"
+          height="1080"
+          controls
+          playsInline
+          preload="none"
+          poster="/og-image.jpg"
+          ref={video}
+        >
+          <source src={videoUrl} type="video/mp4" />
+        </video>
       </div>
       <button
         className="modal-backdrop"
