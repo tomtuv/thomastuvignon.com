7cecf505265f1d580f152b1e18fc70a454cfb251
Thomas Tuvignon <thomas.tuvignon@ultro.fr>
2021-07-15 00:29:55 +0200

    ✨ Add web app manifest

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,25 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#31185a" />
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-          <link rel="icon" href="/icon.png" />
           <link
             rel="preload"
             href="/icomoon.woff"
