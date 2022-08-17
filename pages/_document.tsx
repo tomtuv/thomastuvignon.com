4a695aa24cab9d530038f7b5372b3f248cf61dd9
Thomas Tuvignon <thomas.tuvignon@ultro.fr>
2022-08-17 14:42:11 +0200

    🍱 Update website icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,24 +5,10 @@ export default function Document() {
     <Html>
       <Head>
         <meta name="color-scheme" content="light dark" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
         <link
           href="/Inter-roman.var.woff2"
           type="font/woff2"
