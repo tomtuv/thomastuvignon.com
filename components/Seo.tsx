cfb775a8fff28e46b84b30a1eaa9ece2b49b874d
Thomas Tuvignon <thomas.tuvignon@ultro.fr>
2023-01-15 00:24:22 +0100

    ♻️ Refactor code

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
 
-const SITE_TITLE = "Thomas Tuvignon";
-const SITE_URL = "https://thomastuvignon.com";
-const OG_IMAGE = `${SITE_URL}/og-image.jpg`;
+const SITE_NAME = "Thomas Tuvignon";
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
+const OG_IMAGE_URL = `${SITE_URL}/og-image.jpg`;
 const TWITTER_USERNAME = "@tomtuv";
 
 interface Props {
@@ -13,19 +13,19 @@ interface Props {
 export default function Seo({ title, description }: Props) {
   return (
     <Head>
-      <title>{title ? `${title} | ${SITE_TITLE}` : SITE_TITLE}</title>
+      <title>{title ? `${title} | ${SITE_NAME}` : SITE_NAME}</title>
       <meta name="description" content={description} />
-      <meta name="image" content={OG_IMAGE} />
+      <meta name="image" content={OG_IMAGE_URL} />
       <meta property="og:type" content="website" />
-      <meta property="og:site_name" content={SITE_TITLE} />
+      <meta property="og:site_name" content={SITE_NAME} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={OG_IMAGE} />
+      <meta property="og:image" content={OG_IMAGE_URL} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={TWITTER_USERNAME} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={OG_IMAGE} />
+      <meta name="twitter:image" content={OG_IMAGE_URL} />
     </Head>
   );
 }
--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,11 +1,7 @@
 import type { GetServerSideProps } from "next";
 import { getAllProjectsWithSlug, getAllPagesWithSlug } from "@/lib/api";
 
-export const SITE_URL =
-  process.env.NEXT_PUBLIC_SITE_URL ??
-  `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` ??
-  "http://localhost:3000";
-
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 const xml = String.raw;
 
 export default function SiteMap() {
