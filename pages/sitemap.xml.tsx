439d0651f27af7a42e1474e18ca3a3b09c2487d2
Thomas Tuvignon <thomas.tuvignon@ultro.fr>
2023-01-15 00:07:50 +0100

    🐛 Fix XML

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -23,16 +23,16 @@ export const getServerSideProps: GetServerSideProps = async ({
   ]);
 
   const sitemap = xml`<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.w3.org/TR/xhtml11/xhtml11_schema.html http://www.w3.org/2002/08/xhtml/xhtml1-strict.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/TR/xhtml11/xhtml11_schema.html">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
       <url>
         <loc>${SITE_URL}</loc>
-        <xhtml:link rel="alternate" hreflang="x-default" href="${SITE_URL}"></xhtml:link>
+        <xhtml:link rel="alternate" hreflang="x-default" href="${SITE_URL}" />
         ${locales!
           .map((locale) => {
             return xml`
               <xhtml:link rel="alternate" hreflang="${locale}" href="${`${SITE_URL}${
               locale !== defaultLocale ? `/${locale}` : ""
-            }`}"></xhtml:link>
+            }`}" />
             `;
           })
           .join("")}
@@ -42,13 +42,13 @@ export const getServerSideProps: GetServerSideProps = async ({
           return xml`
             <url>
               <loc>${`${SITE_URL}/projects/${slug}`}</loc>
-              <xhtml:link rel="alternate" hreflang="x-default" href="${`${SITE_URL}/projects/${slug}`}"></xhtml:link>
+              <xhtml:link rel="alternate" hreflang="x-default" href="${`${SITE_URL}/projects/${slug}`}" />
               ${locales!
                 .map((locale) => {
                   return xml`
                     <xhtml:link rel="alternate" hreflang="${locale}" href="${`${SITE_URL}${
                     locale !== defaultLocale ? `/${locale}` : ""
-                  }/projects/${slug}`}"></xhtml:link>
+                  }/projects/${slug}`}" />
                   `;
                 })
                 .join("")}
@@ -61,13 +61,13 @@ export const getServerSideProps: GetServerSideProps = async ({
           return xml`
             <url>
               <loc>${`${SITE_URL}/${slug}`}</loc>
-              <xhtml:link rel="alternate" hreflang="x-default" href="${`${SITE_URL}/${slug}`}"></xhtml:link>
+              <xhtml:link rel="alternate" hreflang="x-default" href="${`${SITE_URL}/${slug}`}" />
               ${locales!
                 .map((locale) => {
                   return xml`
                     <xhtml:link rel="alternate" hreflang="${locale}" href="${`${SITE_URL}${
                     locale !== defaultLocale ? `/${locale}` : ""
-                  }/${slug}`}"></xhtml:link>
+                  }/${slug}`}" />
                   `;
                 })
                 .join("")}
