ee7e68b241767759bbcb53db4e3eb999e92ce1f3
Thomas Tuvignon <thomas.tuvignon@ultro.fr>
2023-01-31 23:04:36 +0100

    ♻️ Refactor imports

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getEntryForPreview } from "../../lib/api";
-import type { HomePage, Page, Project } from "../../lib/types";
+import { getEntryForPreview } from "@/lib/api";
+import type { HomePage, Page, Project } from "@/lib/types";
 
 function resolveUrl(entry: HomePage | Project | Page) {
   switch (entry.__typename) {
