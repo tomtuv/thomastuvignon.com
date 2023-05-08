3ad84675d0814de21dd6276f456bc06d949a129d
Thomas Tuvignon <thomas.tuvignon@ultro.fr>
2023-05-08 18:10:05 +0200

    🐛 Fix internal links

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,17 +1,28 @@
+"use client";
+
 import NextLink from "next/link";
+import { useIntl } from "react-intl";
 import styles from "./Link.module.css";
 
 export default function Link({
   href,
+  hrefLang,
   variant,
   ...props
 }: React.ComponentProps<typeof NextLink> & {
   variant?: "underline" | "underline-inverse";
 }) {
+  const { locale } = useIntl();
+
+  if (href.toString().startsWith("/") && !hrefLang) {
+    href = `/${locale}${href}`;
+  }
+
   return (
     <NextLink
       className={styles.root}
       href={href}
+      hrefLang={hrefLang}
       data-variant={variant}
       {...props}
     />
