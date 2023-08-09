d317b1c44da8b58be8b3b56875566d87f7a8a348
Thomas Tuvignon <thomas.tuvignon@ultro.fr>
2023-08-09 18:34:07 +0200

    🚚 Add redirect for projects route

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [{ protocol: "https", hostname: "images.ctfassets.net" }],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:lang/projects",
+        destination: "/:lang",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
