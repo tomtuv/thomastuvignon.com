94d8e5f3b1b81b2d02e327b12a9a18af783ed00e
Thomas Tuvignon <thomas.tuvignon@ultro.fr>
2024-03-08 15:06:22 +0100

    ♻️ Refactor code

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,4 @@
-import type { TadaDocumentNode } from "gql.tada";
-import request from "graphql-request";
+import { GraphQLClient } from "graphql-request";
 import { unstable_noStore as noStore } from "next/cache";
 import { draftMode } from "next/headers";
 import {
@@ -11,75 +10,78 @@ import {
   projectQuery,
 } from "./queries";
 
-async function fetchAPI<T, V>(query: TadaDocumentNode<T, V>, variables?: V) {
-  const preview =
-    (variables as { preview?: boolean } | undefined)?.preview ?? false;
+const client = new GraphQLClient(
+  `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+  {
+    headers: () => {
+      let isDraftMode: boolean;
 
-  let isDraftMode: boolean;
+      try {
+        isDraftMode = draftMode().isEnabled;
+      } catch {
+        isDraftMode = false;
+      }
 
-  try {
-    isDraftMode = draftMode().isEnabled || preview;
-  } catch {
-    isDraftMode = preview;
-  }
+      if (isDraftMode) {
+        noStore();
+      }
 
-  if (isDraftMode) {
-    noStore();
-  }
-
-  const data = await request(
-    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
-    query,
-    {
-      ...variables,
-      preview: isDraftMode,
+      return {
+        Authorization: `Bearer ${
+          process.env[
+            isDraftMode
+              ? "CONTENTFUL_PREVIEW_ACCESS_TOKEN"
+              : "CONTENTFUL_ACCESS_TOKEN"
+          ]
+        }`,
+      };
     },
-    {
-      Authorization: `Bearer ${
-        process.env[
-          isDraftMode
-            ? "CONTENTFUL_PREVIEW_ACCESS_TOKEN"
-            : "CONTENTFUL_ACCESS_TOKEN"
-        ]
-      }`,
-    }
-  );
-
-  return data;
-}
+  }
+);
 
 export async function getDraftEntry(id: string) {
-  const data = await fetchAPI(draftEntryQuery, { id, preview: true });
+  const data = await client.request(draftEntryQuery, { id, preview: true });
 
   return data?.entryCollection?.items[0];
 }
 
 export async function getHomePage({ locale }: { locale: string }) {
-  const data = await fetchAPI(homePageQuery, { locale });
+  const data = await client.request(homePageQuery, {
+    locale,
+    preview: draftMode().isEnabled,
+  });
 
   return data?.homePageCollection?.items[0];
 }
 
 export async function getProject(slug: string, { locale }: { locale: string }) {
-  const data = await fetchAPI(projectQuery, { slug, locale });
+  const data = await client.request(projectQuery, {
+    slug,
+    locale,
+    preview: draftMode().isEnabled,
+  });
 
   return data?.projectCollection?.items[0];
 }
 
 export async function getAllProjectsWithSlug() {
-  const data = await fetchAPI(allProjectsWithSlugQuery);
+  const data = await client.request(allProjectsWithSlugQuery);
 
   return data?.projectCollection?.items ?? [];
 }
 
 export async function getPage(slug: string, { locale }: { locale: string }) {
-  const data = await fetchAPI(pageQuery, { slug, locale });
+  const data = await client.request(pageQuery, {
+    slug,
+    locale,
+    preview: draftMode().isEnabled,
+  });
 
   return data?.pageCollection?.items[0];
 }
 
 export async function getAllPagesWithSlug() {
-  const data = await fetchAPI(allPagesWithSlugQuery);
+  const data = await client.request(allPagesWithSlugQuery);
 
   return data?.pageCollection?.items ?? [];
 }
