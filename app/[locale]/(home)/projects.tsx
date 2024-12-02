f1922e5f80e8b2714131f33f3ed2f9bf15688c7a
Thomas Tuvignon <thomas.tuvignon@ultro.fr>
2024-12-02 18:32:54 +0100

    💥 Migrate `framer-motion` to `motion`

--- app/[locale]/(home)/projects.tsx
+++ app/[locale]/(home)/projects.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import { useId } from "react";
 import FormattedMessage from "@/components/formatted-message";
 import Image from "@/components/image";
