917e87949e11ee50ad1d11130ae572ae7279196c
Thomas Tuvignon <thomas.tuvignon@ultro.fr>
2025-10-15 19:11:09 +0200

    🐛 Fix `SITE_URL` constant

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,8 @@
 import en from "@/locales/en.json";
 import fr from "@/locales/fr.json";
 
-export const SITE_URL = process.env.SITE_URL ?? "http://localhost:3000";
+export const SITE_URL =
+  process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
 export const SITE_NAME = "Thomas Tuvignon";
 export const TWITTER_USERNAME = "@tomtuv";
 export const LOCALES = ["fr", "en"];
